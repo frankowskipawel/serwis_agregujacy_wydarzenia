593570d04f7ea502423f9efe896ac7d926f5ff97
Paweł <frankowski.pawel@gmail.com>
2020-06-27 12:31:04 +0200

    Dodano role do usera.

--- src/main/java/com/sda/model/User.java
+++ src/main/java/com/sda/model/User.java
@@ -5,20 +5,25 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "`user`")
+@Table(name = "user")
 public class User {
 
     @Id
     @GeneratedValue
+    @Column(name = "user_id")
     private int id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 
 }
