6bee7c9cd06be70c310452d2274beb7931b1de1b
Rafał Załęski <rafalzaleski@gmail.com>
2020-06-27 12:59:11 +0200

    Commentary model

--- src/main/java/com/sda/model/Event.java
+++ src/main/java/com/sda/model/Event.java
@@ -10,25 +10,21 @@ import java.util.Date;
 
 @Entity
 @Data
-@Table(name="`event`")
+@Table(name="event")
 public class Event {
 
     @Id
     @GeneratedValue
     @Column(name="EV_ID")
     private int id;
-    @Column(name="EV_TITLE")
     private String title;
-    @Column(name="EV_DESCRIPTION")
     private String description;
-    @Column(name="EV_CITY")
     private String city;
-    @Column(name="EV_DATE")
     private Date date;
-    @Column(name="EV_TIME")
     private Time time;
-    @Column(name="EV_USER")
+    @ManyToOne
     private User user;
+
 
 
 }
