6c3f88b0378d339ea899736aaececc74629c125b
Paweł <frankowski.pawel@gmail.com>
2020-06-27 10:39:44 +0200

    Initial commit

---
+++ src/main/java/com/sda/serwis_agregujacy_wydarzenia/SerwisAgregujacyWydarzeniaApplication.java
@@ -0,0 +1,13 @@
+package com.sda.serwis_agregujacy_wydarzenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisAgregujacyWydarzeniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwisAgregujacyWydarzeniaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/serwis_agregujacy_wydarzenia/SerwisAgregujacyWydarzeniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.serwis_agregujacy_wydarzenia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisAgregujacyWydarzeniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
