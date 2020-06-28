e0cc4e0d2dc8d81a75ae70ee2094e3ebb30dd43f
Paweł <frankowski.pawel@gmail.com>
2020-06-28 16:22:17 +0200

    Added Events List in home view.

--- src/main/java/com/sda/service/EventService.java
+++ src/main/java/com/sda/service/EventService.java
@@ -3,9 +3,11 @@ package com.sda.service;
 import com.sda.model.Event;
 import com.sda.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-
+import java.util.List;
 
 
 @Service
@@ -15,7 +17,15 @@ public class EventService {
     private EventRepository eventRepository;
 
     public void createEvent(Event event){
-
       eventRepository.save(event);
     }
+
+    public List<Event> findAll(){
+        return eventRepository.findAll();
+    }
+
+    public Page<Event> findAllPagination(Pageable pageable) {
+        return eventRepository.findAll(pageable);
+    }
+
 }
