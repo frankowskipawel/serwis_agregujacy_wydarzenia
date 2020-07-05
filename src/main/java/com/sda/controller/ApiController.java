6581b7ef8acdd75414c9b8f82f34c7d1088351e6
Paweł <frankowski.pawel@gmail.com>
2020-07-05 12:25:11 +0200

    commit

--- src/main/java/com/sda/controller/ApiController.java
+++ src/main/java/com/sda/controller/ApiController.java
@@ -1,14 +1,12 @@
 package com.sda.controller;
 
 import com.sda.entity.Event;
-import com.sda.modelAPI.EventAPI;
 import com.sda.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -20,8 +18,8 @@ public class ApiController {
 
 
     @GetMapping("/events")
-    public List<EventAPI> getEvents(){
+    public List<Event> getEvents(){
 
-        return eventService.findAllPastEvent("",new Date());
+        return eventService.findAll();
     }
 }
