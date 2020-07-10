5b08a12d9a2874d452c07ceae40aa9375a440966
Paweł <frankowski.pawel@gmail.com>
2020-07-10 15:42:21 +0200

    Add max letters in password.

--- src/main/java/com/sda/controller/UserController.java
+++ src/main/java/com/sda/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,7 +62,14 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(@Valid @ModelAttribute("user") User user, BindingResult result) {
+    public String register(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) {
+
+        if (user.getPassword().length() > 20) {
+            model.addAttribute("passwordMaxLength", "Passwod must be a max 20 letters");
+            ObjectError error = new ObjectError("password", "");
+            result.addError(error);
+        }
+
         if (result.hasErrors()) {
             return "register";
         } else {
